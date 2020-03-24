6f0eb2c0648183a28cf354481af8f611e0e9b17b
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-24 14:59:29 -0500

    login form setup

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../loginForm/LoginForm.css';
 
 function LoginForm() {
     return(
         <div>
-            <h1>Login Form working</h1>
+            <form>
+                <span>
+                    <input placeholder='Username' id='username'/>
+                    <label htmlFor='username'>Username</label>
+                    <input placeholder='Password' id='password'/>
+                    <label htmlFor='password'>Password</label>
+                </span>
+
+                <span className='submitBtn'>
+                    <button type='submit' className='btn'>Submit</button>
+                </span>
+            </form>
         </div>
     )
 }
