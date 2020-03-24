a64626b7ae65b8c0479f3da132331c9370db3517
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-24 10:08:02 -0500

    react componenets setup

---
+++ src/components/screenShots/ScreenShots.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ScreenShots() {
+    return(
+        <div>
+            <h1>Screen shots working</h1>
+        </div>
+    )
+}
+
+export default ScreenShots;
---
+++ src/components/sideMenuBar/SideMenuBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SideMenuBar() {
+    return(
+        <div>
+            <h1>Side Menu Bar working</h1>
+        </div>
+    )
+}
+
+export default SideMenuBar;
