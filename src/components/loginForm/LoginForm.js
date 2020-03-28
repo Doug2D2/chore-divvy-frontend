1da6ed96807539498e1432bc7f82a33d2d73af28
Natalie Vasquez <natalievasquez11@gmail.com>
2020-03-27 19:44:42 -0500

    setup fetch in login form

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -3,6 +3,38 @@ import '../loginForm/LoginForm.css';
 import { Link } from 'react-router-dom';
 
 class LoginForm extends Component {
+    state = {
+        isLoggedIn: false,
+        loginCredentials: {},
+        usernameInput: "",
+        passwordInput: ""
+    }
+
+    handleSubmitLoginForm(username, password) {
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => console.log(err));
+
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({ usernameInput: event.target.value });
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({ passwordInput: event.target.value });
+    }
 
     render() {
         return(
@@ -11,15 +43,24 @@ class LoginForm extends Component {
                 <span className='formContainer'>
                     <form>
                         <span>
-                            <input placeholder='Username' id='username' className='inputStyle'/>
+                            <input placeholder='Username' id='username' className='inputStyle' 
+                            value={this.state.usernameInput}
+                            onChange={this.handleUsernameChange} 
+                            />
                             <label htmlFor='username'>Username</label>
-                            <input placeholder='Password' id='password' className='inputStyle'/>
+                            <input placeholder='Password' id='password' className='inputStyle' 
+                            value={this.state.passwordInput}
+                            onChange={this.handlePasswordChange}
+                            />
                             <label htmlFor='password'>Password</label>
                         </span>
 
                             <div className='submitBtn'>
                                 <Link to='/dashboard'>
-                                    <button type='submit' className='btn btn-large indigo'>Submit</button>
+                                    <button type='submit' className='btn btn-large indigo' 
+                                    onClick={() => this.handleSubmitLoginForm(this.state.usernameInput, this.state.passwordInput)}>
+                                        Submit
+                                    </button>
                                 </Link>
                             </div>
 
