858d9274ac239681142aff64ebacf7f15546d6eb
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-05 21:11:23 -0500

    forgot password component form update

--- src/components/forgotPassword/ForgotPassword.js
+++ src/components/forgotPassword/ForgotPassword.js
@@ -5,8 +5,20 @@ const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 class ForgotPassword extends Component {
     render() {
         return (
-            <div>
-                <h1>Forgot Password Works!</h1>
+            <div className='row'>
+                <span className='formContainer'>
+                    <form>
+                        <h5>Forgot Password?</h5>
+                        <p>Enter your email and we'll send you your new password.</p>
+                        <br/>
+                        <input placeholder='Email' id='username' className='inputStyle'/>
+                        <label htmlFor='username'>Email</label>
+
+                        <button type='submit' className='btn btn-large indigo'>
+                            Reset Password
+                        </button>
+                    </form>
+                </span>
             </div>
         )
     }
