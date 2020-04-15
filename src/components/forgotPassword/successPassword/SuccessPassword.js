4bd91cd94f9f9dfba8a64fe6d77d36eeae389a28
Natalie Vasquez <natalievasquez11@gmail.com>
2020-04-15 13:30:14 -0500

    header design

--- src/components/forgotPassword/successPassword/SuccessPassword.js
+++ src/components/forgotPassword/successPassword/SuccessPassword.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../successPassword/SuccessPassword.css';
 
 function SuccessPassword() {
     return (
         <div className='row'>
-            <span className='formContainer'>
+            <span className='formContainer successContainer'>
                 <form>
                     <h5 className='forgotPwText'>An email has been sent.</h5>
                     <h5><Link to='/'>Login here</Link></h5>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../header/Header.css';
 
 function Header() {
     return(
         <div>
-            <h1>Chore Divvy</h1>
+              <nav>
+                <div className="nav-wrapper">
+                <a href="/" className="brand-logo center"><h1 className='brand-logo-title'>Chore Divvy</h1></a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/sign-up-form">Sign Up</a></li>
+                </ul>
+                </div>
+            </nav>
+            
         </div>
     )
 }
