d1ee587d22ba7427e1fc2f0da750606311a85cdf
Natalie Vasquez <natalievasquez11@gmail.com>
2020-11-02 10:26:42 -0600

    Fixed /get-users API call in chore component

--- src/components/dashboard/chores/Chores.js
+++ src/components/dashboard/chores/Chores.js
@@ -96,7 +96,7 @@ class Chores extends Component {
         
         if(choreName && choreStatus && choreCatId) {
             if(choreAssigneeUsername) {
-                fetch(`${baseUrl}`)
+                fetch(`${baseUrl}/get-users`)
                 .then(res => res.json())
                 .then(users => {
                     
